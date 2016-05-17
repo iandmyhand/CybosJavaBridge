1edb8b751a291b6c58122605d985f755f5fc6ec4
SeomGi, Han <iandmyhand@gmail.com>
2016-05-18 00:24:44 +0900

    Add CpCybosTest class.

--- cybosjavabridge/src/test/java/com/blackvelvet/cybos/bridge/cputil/CpCybosTest.java
+++ cybosjavabridge/src/test/java/com/blackvelvet/cybos/bridge/cputil/CpCybosTest.java
@@ -1,38 +1,22 @@
 package com.blackvelvet.cybos.bridge.cputil;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class CpCybosTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public CpCybosTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Before;
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( CpCybosTest.class );
-    }
+public class CpCybosTest {
+	
+	ICpCybos cpCybos;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Before
+	public void setUp() throws Exception {
+		cpCybos = ClassFactory.createCpCybos();
+	}
+
+	@Test
+	public void test() {
+		assertEquals(1, cpCybos.isConnect());
+	}
+
 }
